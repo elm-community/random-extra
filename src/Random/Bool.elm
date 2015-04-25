7a0e919645bd9622446291f941f9ebd76c03501d
Hassan Hayat <hassan.hayat7@gmail.com>
2015-04-25 04:09:59 +0100

    Redefined bool generator in terms of frequency

--- src/Random/Bool.elm
+++ src/Random/Bool.elm
@@ -4,19 +4,13 @@ module Random.Bool where
 @docs bool
 -}
 
-import Random exposing (Generator, generate, customGenerator, int)
-
+import Random       exposing (Generator)
+import Random.Extra exposing (frequency, constant)
 {-| Random Bool generator
 -}
 bool : Generator Bool
 bool =
-  customGenerator
-    (\seed ->
-        let (value1, seed1) = generate (int 0 1) seed
-            (value2, seed2) = generate (int 0 1) seed1
-        in
-          if (value1 + value2) == 1
-          then
-            (False, seed2)
-          else
-            (True, seed2))
+  frequency
+    [ (1, constant True)
+    , (1, constant False)
+    ] (constant True)
