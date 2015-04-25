27ccaf67c7fec926da851ccc08bc2b98c17fdec1
Hassan Hayat <hassan.hayat7@gmail.com>
2015-04-25 16:23:04 +0100

    Added `withDefault` and `withDefaultGenerator`

--- src/Random/Maybe.elm
+++ src/Random/Maybe.elm
@@ -6,7 +6,8 @@ module Random.Maybe where
 -}
 
 import Random       exposing (Generator)
-import Random.Extra exposing (constant, map, frequency)
+import Random.Extra exposing (constant, map, frequency, flatMap)
+import Maybe
 
 {-| Generate a Maybe from a generator. Will generate Nothings 50% of the time.
 -}
@@ -16,3 +17,15 @@ maybe generator =
     [ (1, constant Nothing)
     , (1, map Just generator)
     ] (constant Nothing)
+
+{-| Generate values from a maybe generator or a default value.
+-}
+withDefault : a -> Generator (Maybe a) -> Generator a
+withDefault value generator =
+  map (Maybe.withDefault value) generator
+
+{-| Generate values from a maybe generator or a default generator.
+-}
+withDefaultGenerator : Generator a -> Generator (Maybe a) -> Generator a
+withDefaultGenerator default generator =
+  flatMap (flip withDefault generator) default
