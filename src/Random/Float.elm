e005c6abbc7cb6b0f7afbfd007aac93bef38f002
Bug <bug@dlestra.ws>
2016-08-04 11:43:02 -0600

    missing )

--- src/Random/Float.elm
+++ src/Random/Float.elm
@@ -60,5 +60,5 @@ a uniform distribution)
 -}
 standardNormal : Generator Float
 standardNormal = Random.map2
-  (\u theta -> sqrt (-2 * logBase e (1 - u) * cos theta)
+  (\u theta -> sqrt (-2 * logBase e (1 - u) * cos theta))
   (Random.float 0 1) (Random.float 0 (2 * pi))
